7008abe0d78b5bb3c327344a6fe3cb86909d6a1e
SungHo_Kim <82712671+SungHo3268@users.noreply.github.com>
2021-05-09 17:13:18 +0900

    renew

--- subword/scripts/run_preprocess.py
+++ subword/scripts/run_preprocess.py
@@ -4,56 +4,49 @@ import json
 import pickle
 import os
 import sys
+from collections import Counter
 sys.path.append(os.getcwd())
 from src.preprocess import *
+from src.functions import *
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--mode', type=str, default='dummy')
-parser.add_argument('--f_start', type=int, default=1, help="1~99")
-parser.add_argument('--f_end', type=int, default=99, help='1~99')
 parser.add_argument('--least_freq', type=int, default=5)
 parser.add_argument('--min_n', type=int, default=3)
 parser.add_argument('--max_n', type=int, default=6)
-parser.add_argument('--seg', type=int, default=5, help="How many numbers do you divide the file into?")
+parser.add_argument('--seg', type=int, default=10, help="How many numbers do you divide the file into?")
 
 args = parser.parse_args()
 
-with open('datasets/preprocessed/preprocess_argparse.json', 'w') as f:
+with open('datasets/wiki9/preprocessed/preprocess_argparse.json', 'w') as f:
     json.dump(args.__dict__, f)
 
 
 ############################################################################
-# make word dictionary
-word_to_id, id_to_word, vocabulary = make_wordDict(args.f_start, args.f_end, args.least_freq)
-with open('datasets/preprocessed/wordDict.pkl', 'wb') as fw:
-    pickle.dump((word_to_id, id_to_word, vocabulary), fw)
-print("Succeed to save (word_to_id, id_to_word, vocabulary) to 'wordDict.pkl'.")
-print("vocabulary length : ", len(vocabulary))
-
-
-# load the dictionary for making sentence list
-with open('datasets/preprocessed/wordDict.pkl', 'rb') as fr:
+# # make word dictionary
+# word_to_id, id_to_word, vocabulary = make_wordDict(file, args.least_freq)
+# with open('datasets/wiki9/preprocessed/wordDict.pkl', 'wb') as fw:
+#     pickle.dump((word_to_id, id_to_word, vocabulary), fw)
+# print("Succeed to save (word_to_id, id_to_word, vocabulary) to 'wordDict.pkl'.")
+# print("vocabulary length : ", len(vocabulary))
+with open('datasets/wiki9/preprocessed/wordDict.pkl', 'rb') as fr:
     word_to_id, id_to_word, vocabulary = pickle.load(fr)
 
 
-# make subword dictionary which (word + subword)
-subword_to_id, id_to_subword = make_subwordDict(vocabulary, args.min_n, args.max_n)
-with open('datasets/preprocessed/subwordDict.pkl', 'wb') as fw:
-    pickle.dump((subword_to_id, id_to_subword), fw)
-print("Succeed to save (subword_to_id, id_to_subword) to 'subwordDict.pkl'.")
-print("vocabulary length : ", len(subword_to_id))
+# # make subword dictionary which (word + subword)
+# subword_to_id, id_to_subword = make_subwordDict(vocabulary, args.min_n, args.max_n)
+# with open('datasets/wiki9/preprocessed/subwordDict.pkl', 'wb') as fw:
+#     pickle.dump((subword_to_id, id_to_subword), fw)
+# print("Succeed to save (subword_to_id, id_to_subword) to 'subwordDict.pkl'.")
+# print("vocabulary length : ", len(subword_to_id))
+with open('datasets/wiki9/preprocessed/subwordDict.pkl', 'rb') as fr:
+    subword_to_id, id_to_subword = pickle.load(fr)
 
 
-# make sentence list excepted non_vocab
-batch = args.f_end//args.seg + 1
-for i in range(args.seg):
-    if i == 0:
-        sentence_list = make_sentences(word_to_id, args.f_start, batch*(i+1))
-    else:
-        sentence_list = make_sentences(word_to_id, batch*i, batch*(i+1))
-    with open('datasets/preprocessed/sentence_list/sentence_list_{}.pkl'.format(i), 'wb') as fw:
-        pickle.dump(sentence_list, fw)
-    print("Succeed to make sentence list{}.".format(i))
-    print("The length of the sentence list{} is ".format(i), len(sentence_list))
-
+# make corpus
+corpus = make_corpus(file, word_to_id)
+with open('datasets/wiki9/preprocessed/corpus/corpus_all.pkl', 'wb') as fw:
+    pickle.dump(np.array(corpus), fw)
+print("Succeed to make corpus_all.")
+print("The length of corpus is ", len(corpus))
